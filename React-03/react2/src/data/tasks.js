494541656443aa146b107ffdba9540cde249acbb
Antonio4350 <antoniocruzvalletto00@gmail.com>
2025-08-13 20:01:39 -0300

    act

--- React-03/react2/src/data/tasks.js
+++ React-03/react2/src/data/tasks.js
@@ -2,15 +2,15 @@ export const initialTasks = [
   {
     id: 1,
     title: "Comprar pan",
-    description: "Ir a la panadería y comprar pan fresco.",
-    shortDescription: "Ir a la panadería",
+    description: "Ir a la panaderia y comprar pan fresco.",
+    shortDescription: "Ir a la panaderia",
     createdAt: "2025-08-13",
     completed: false
   },
   {
     id: 2,
     title: "Estudiar React",
-    description: "Repasar componentes, hooks y routing.",
+    description: "Repasar componentes.",
     shortDescription: "Repasar React",
     createdAt: "2025-08-12",
     completed: true
